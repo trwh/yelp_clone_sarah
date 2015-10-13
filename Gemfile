788c442a8a523be570c62516921db209b1a4908a
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-13 17:54:41 +0100

    Validation test added for restaurants

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'shoulda'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'rspec-collection_matchers'
+
 end
 
 group :development do
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
-  describe Restaurant, type: :model do
-    it { is_expected.to have_many :reviews }
+
+describe Restaurant, :type => :model do
+  it { is_expected.to have_many :reviews }
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
   end
+end
 
   describe Review, type: :model do
     it { is_expected.to belong_to :restaurant }
