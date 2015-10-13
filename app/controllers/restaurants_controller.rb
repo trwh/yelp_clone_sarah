1efb8e01b7b66db741fb5e4e6ae6a2c476133b30
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-13 11:13:39 +0100

    Added test for creating a restaurant - tests passing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,17 @@ class RestaurantsController < ApplicationController
   def index
     # raise 'Hello from the index action'
   end
+
+  def new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
 end
