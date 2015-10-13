0bdcc62f5bf6f4da1f1937291e955fb6546cca88
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-13 15:19:01 +0100

    Update and edit methods added

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -3,17 +3,14 @@ require 'rails_helper'
 feature 'restaurants' do
 
   context 'no restaurants have been added' do
-
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
-
   end
 
   context 'restaurants have been added' do
-
     before do
       Restaurant.create(name: 'KFC')
     end
@@ -23,11 +20,9 @@ feature 'restaurants' do
       expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
     end
-
   end
 
   context 'creating restaurants' do
-
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
       visit '/restaurants'
       click_link 'Add a restaurant'
@@ -36,20 +31,29 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
-
   end
 
   context 'viewing restaurants' do
-
     let!(:kfc){Restaurant.create(name:'KFC')}
-
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'KFC'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
+  end
 
-end
+  context 'editing restaurants' do
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'let a user edit a restaurant' do
+     visit '/restaurants'
+     click_link 'Edit KFC'
+     fill_in 'Name', with: 'Kentucky Fried Chicken'
+     click_button 'Update Restaurant'
+     expect(page).to have_content 'Kentucky Fried Chicken'
+     expect(current_path).to eq '/restaurants'
+    end
+  end
 
 end
