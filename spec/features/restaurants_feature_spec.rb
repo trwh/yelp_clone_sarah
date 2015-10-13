9e4b838067a6f3dd99dd53cc16b25c864070e556
Sarah Kristine Pedersen <mail@sarahkp.dk>
2015-10-13 16:24:03 +0100

    Reviews added

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -57,14 +57,14 @@ feature 'restaurants' do
   end
 
   context 'deleting restaurants' do
-  before { Restaurant.create name: 'KFC' }
+    before { Restaurant.create name: 'KFC' }
 
-  scenario 'removes a restaurant when a user clicks a delete link' do
-    visit '/restaurants'
-    click_link 'Delete KFC'
-    expect(page).not_to have_content 'KFC'
-    expect(page).to have_content 'Restaurant deleted successfully'
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
   end
-end
 
 end
